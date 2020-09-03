faf0100a641eaaf296d9f7f66dbe5745c1a43144
Maximiliano López <maxi.lopez.ramirez@gmail.com>
2020-09-03 11:36:48 -0400

    Add design for succes view

--- app/controllers/spree/khipu_controller.rb
+++ app/controllers/spree/khipu_controller.rb
@@ -1,6 +1,7 @@
 module Spree
   class KhipuController < Spree::BaseController
     protect_from_forgery except: [:notify]
+    layout 'spree/layouts/redirect', only: :success
 
     def success
       @payment = Spree::Payment.where(number: params[:payment]).last
@@ -33,8 +34,8 @@ module Spree
 
     def notify
       notification_token = params["notification_token"]
-      client    = Khipu::PaymentsApi.new
-      response  = client.payments_get(notification_token)
+      client = Khipu::PaymentsApi.new
+      response = client.payments_get(notification_token)
 
       payment = Spree::Payment.find_by(number: response.transaction_id)
 
@@ -44,10 +45,9 @@ module Spree
           payment.complete!
 
           @khipu_receipt = Spree::KhipuPaymentReceipt.where(transaction_id: payment.number).last
-          @khipu_receipt.update(params.select{ |k,v| @khipu_receipt.attributes.keys.include? k })
+          @khipu_receipt.update(params.select { |k, v| @khipu_receipt.attributes.keys.include? k })
           @khipu_receipt.save!
-        else
-          payment.failure!
+        else payment.failure!
         end
       end
       head :ok
@@ -55,8 +55,7 @@ module Spree
       head :unprocessable_entity
     end
 
-    def completion_route(order, custom_params = nil)
-      spree.order_path(order, custom_params)
+    def completion_route(order, custom_params = nil) spree.order_path(order, custom_params)
     end
   end
 end
